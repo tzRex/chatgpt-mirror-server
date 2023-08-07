551183bfbc32242e2d7b57a3e9136d936cc6809c
floveRex <2857348582@qq.com>
2023-08-07 20:40:56 +0800

    修改脚本内容，不提交远程docker库

--- release.sh
+++ release.sh
@@ -7,6 +7,3 @@ gf docker main.go -t xyhelper/chatgpt-mirror-server:latest
 # 修改镜像标签为当前日期时间
 time=$(date "+%Y%m%d%H%M%S")
 docker tag xyhelper/chatgpt-mirror-server:latest xyhelper/chatgpt-mirror-server:$time
-# 推送镜像到docker hub
-docker push xyhelper/chatgpt-mirror-server:latest
-docker push xyhelper/chatgpt-mirror-server:$time
